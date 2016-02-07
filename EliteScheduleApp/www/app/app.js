569e17244c756314fd039f1a186cb7320e1f4af0
Jose Antonio Sanchez <jasancheg@gmail.com>
2016-02-06 19:22:50 -0600

    create new pages, adding routing for side menu and tabs

--- EliteScheduleApp/www/app/app.js
+++ EliteScheduleApp/www/app/app.js
@@ -12,7 +12,7 @@ angular
 				// org.apche.cordova.statusbar required
 				StatusBar.styleDefault();
 			}
-		});	
+		});
 	})
 
 	.config(function($stateProvider, $urlRouterProvider) {
@@ -40,9 +40,58 @@ angular
 			})
 			.state('app', {
 				url: '/app',
+				abstract: true,
 				templateUrl: 'app/layout/menu-layout.html'
+			})
+			.state('app.teams', {
+				url: '/teams',
+				views: {
+					'mainContent': {
+						templateUrl: 'app/teams/teams.html'
+					}
+				}
+			})
+			.state('app.team-detail', {
+				url: '/teams/:id',
+				views: {
+					'mainContent': {
+						templateUrl: 'app/teams/team-detail.html'
+					}
+				}
+			})
+			.state('app.game', {
+				url: '/game/:id',
+				views: {
+					'mainContent': {
+						templateUrl: 'app/game/game.html'
+					}
+				}
+			})
+			.state('app.standings', {
+				url: '/standings',
+				views: {
+					'mainContent': {
+						templateUrl: 'app/standings/standings.html'
+					}
+				}
+			})
+			.state('app.locations', {
+				url: '/locations',
+				views: {
+					'mainContent': {
+						templateUrl: 'app/locations/locations.html'
+					}
+				}
+			})
+			.state('app.rules', {
+				url: '/rules',
+				views: {
+					'mainContent': {
+						templateUrl: 'app/rules/rules.html'
+					}
+				}
 			});
 
 		// if none of the above states are matched, use this as the fallback
-		$urlRouterProvider.otherwise('/home');
+		$urlRouterProvider.otherwise('/app/teams');
 	});
