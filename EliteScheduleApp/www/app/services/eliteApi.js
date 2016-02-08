e0b856c3739a551a1e4e8677674e38efb9c093af
Jose Antonio Sanchez <jasancheg@gmail.com>
2016-02-07 23:20:19 -0600

    implementing caches

--- EliteScheduleApp/www/app/services/eliteApi.js
+++ EliteScheduleApp/www/app/services/eliteApi.js
@@ -3,42 +3,65 @@
 
     angular
         .module('eliteApp')
-        .factory('eliteApi', ['$http', '$q', '$ionicLoading',  eliteApi])
-    function eliteApi($http, $q, $ionicLoading) {
+        .factory('eliteApi', ['$http', '$q', '$ionicLoading', 'CacheFactory', eliteApi])
+    function eliteApi($http, $q, $ionicLoading, CacheFactory) {
         var currentLeagueId;
+
+        self.leaguesCache = CacheFactory.get("leaguesCache");
+        self.leagueDataCache = CacheFactory.get("leagueDataCache");
 
         function getLeagues() {
             var deferred = $q.defer();
-            $ionicLoading.show({template: 'Loading...'});
-            $http
-                .get("http://elite-schedule.net/api/leaguedata")
-                .success(function(data) {
-                    $ionicLoading.hide();
-                    deferred.resolve(data);
-                })
-                .error(function() {
-                    $ionicLoading.hide();
-                    console.log("Error while making HTTP call.");
-                    deferred.reject();
-                });
+            var cacheKey = 'leagues';
+            var leaguesData = self.leaguesCache.get(cacheKey);
+
+            if(leaguesData) {
+                console.log("Found data inside cache", leaguesData);
+                deferred.resolve(leaguesData);
+            } else {
+                $ionicLoading.show({template: 'Loading...'});
+                $http
+                    .get("http://elite-schedule.net/api/leaguedata")
+                    .success(function(data) {
+                        $ionicLoading.hide();
+                        console.log("Received data via HTTP");
+                        self.leaguesCache.put(cacheKey, data);
+                        deferred.resolve(data);
+                    })
+                    .error(function() {
+                        $ionicLoading.hide();
+                        console.log("Error while making HTTP call.");
+                        deferred.reject();
+                    });
+            }
+
             return deferred.promise;
         }
 
         function getLeagueData() {
             var deferred = $q.defer();
-            $ionicLoading.show({template: 'Loading...'});
-            $http
-                .get("http://elite-schedule.net/api/leaguedata/" + currentLeagueId)
-                .success(function(data, status) {
-                    $ionicLoading.hide();
-                    console.log("Received schedule data via HTTP.", data, status);
-                    deferred.resolve(data);
-                })
-                .error(function() {
-                    $ionicLoading.hide();
-                    console.log("Error while making HTTP call.");
-                    deferred.reject();
-                });
+            var cacheKey = 'leagueData-' + currentLeagueId;
+            var leagueData = self.leagueDataCache.get(cacheKey);
+
+            if(leagueData) {
+                console.log("Found data in cache", leagueData);
+                deferred.resolve(leagueData);
+            } else {
+                $ionicLoading.show({template: 'Loading...'});
+                $http
+                    .get("http://elite-schedule.net/api/leaguedata/" + currentLeagueId)
+                    .success(function(data, status) {
+                        $ionicLoading.hide();
+                        console.log("Received schedule data via HTTP.", data, status);
+                        self.leagueDataCache.put(cacheKey, data);
+                        deferred.resolve(data);
+                    })
+                    .error(function() {
+                        $ionicLoading.hide();
+                        console.log("Error while making HTTP call.");
+                        deferred.reject();
+                    });
+            }
             return deferred.promise;
         }
 
