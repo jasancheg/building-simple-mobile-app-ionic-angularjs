235f1d88147d208f830bc172d6fa7684faf26d32
Jose Antonio Sanchez <jasancheg@gmail.com>
2016-02-07 22:33:37 -0600

    add loading popup status

--- EliteScheduleApp/www/app/services/eliteApi.js
+++ EliteScheduleApp/www/app/services/eliteApi.js
@@ -3,18 +3,21 @@
 
     angular
         .module('eliteApp')
-        .factory('eliteApi', ['$http', '$q', eliteApi])
-    function eliteApi($http, $q) {
+        .factory('eliteApi', ['$http', '$q', '$ionicLoading',  eliteApi])
+    function eliteApi($http, $q, $ionicLoading) {
         var currentLeagueId;
 
         function getLeagues() {
             var deferred = $q.defer();
+            $ionicLoading.show({template: 'Loading...'});
             $http
                 .get("http://elite-schedule.net/api/leaguedata")
                 .success(function(data) {
+                    $ionicLoading.hide();
                     deferred.resolve(data);
                 })
                 .error(function() {
+                    $ionicLoading.hide();
                     console.log("Error while making HTTP call.");
                     deferred.reject();
                 });
@@ -23,13 +26,16 @@
 
         function getLeagueData() {
             var deferred = $q.defer();
+            $ionicLoading.show({template: 'Loading...'});
             $http
                 .get("http://elite-schedule.net/api/leaguedata/" + currentLeagueId)
                 .success(function(data, status) {
+                    $ionicLoading.hide();
                     console.log("Received schedule data via HTTP.", data, status);
                     deferred.resolve(data);
                 })
                 .error(function() {
+                    $ionicLoading.hide();
                     console.log("Error while making HTTP call.");
                     deferred.reject();
                 });
